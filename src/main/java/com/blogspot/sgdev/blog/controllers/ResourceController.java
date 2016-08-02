849e615d2716e00c8b4390b7e80eec5632fac236
Thomas Tesche <thomas.tesche@idealo.de>
2016-08-02 09:47:01 +0200

    Format code

--- src/main/java/com/blogspot/sgdev/blog/controllers/ResourceController.java
+++ src/main/java/com/blogspot/sgdev/blog/controllers/ResourceController.java
@@ -9,41 +9,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/resources")
 public class ResourceController {
-    
-	@PreAuthorize("hasRole('ROLE_USER')")
-    @RequestMapping(value="user", method=RequestMethod.GET)
-    public String helloUser() {
-        return "hello user";
-    }
-    
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-    @RequestMapping(value="admin", method=RequestMethod.GET)
-    public String helloAdmin() {
-        return "hello admin";
-    }
-    
-	@PreAuthorize("hasRole('ROLE_CLIENT')")
-    @RequestMapping(value="client", method=RequestMethod.GET)
-    public String helloClient() {
-        return "hello user authenticated by normal client";
-    }
-	
-	@PreAuthorize("hasRole('ROLE_TRUSTED_CLIENT')")
-	@RequestMapping(value="trusted_client", method=RequestMethod.GET)
-	public String helloTrustedClient() {
-		return "hello user authenticated by trusted client";
-	}
-	
-	@RequestMapping(value="principal", method=RequestMethod.GET)
-	public Object getPrincipal() {
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		return principal;
-	}
-	
-	@RequestMapping(value="roles", method=RequestMethod.GET)
-	public Object getRoles() {
-		return SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-	}
 
+   @PreAuthorize("hasRole('ROLE_USER')")
+   @RequestMapping(value = "user", method = RequestMethod.GET)
+   public String helloUser() {
+      return "hello user";
+   }
+
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
+   @RequestMapping(value = "admin", method = RequestMethod.GET)
+   public String helloAdmin() {
+      return "hello admin";
+   }
+
+   @PreAuthorize("hasRole('ROLE_CLIENT')")
+   @RequestMapping(value = "client", method = RequestMethod.GET)
+   public String helloClient() {
+      return "hello user authenticated by normal client";
+   }
+
+   @PreAuthorize("hasRole('ROLE_TRUSTED_CLIENT')")
+   @RequestMapping(value = "trusted_client", method = RequestMethod.GET)
+   public String helloTrustedClient() {
+      return "hello user authenticated by trusted client";
+   }
+
+   @RequestMapping(value = "principal", method = RequestMethod.GET)
+   public Object getPrincipal() {
+      Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      return principal;
+   }
+
+   @RequestMapping(value = "roles", method = RequestMethod.GET)
+   public Object getRoles() {
+      return SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+   }
 
 }
